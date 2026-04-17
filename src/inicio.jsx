a53a013d7c3b1681cb76b754c7d7368b5da99d1d
veronica <veronica.granados@correounivalle.edu.co>
2026-04-17 00:20:01 -0500

    arreglo de los botones

--- src/inicio.jsx
+++ src/inicio.jsx
@@ -1,5 +1,10 @@
 import "./App.css";
 import logo from "./assets/logo.png";
+import paciente from "./assets/paciente.png";
+import doctor from "./assets/doctor.png";
+import admin from "./assets/admin.png";
+import sup from "./assets/sup.png";
+
 function Inicio() {
   return (
     <div className="container">
@@ -9,14 +14,36 @@ function Inicio() {
         <p>Sistema Web de Gestión de Citas Médicas</p>
         <h6>Universidad del Valle - Desarrollo de Software I </h6>
 
-        <div className="botones">
-          <button>Paciente</button>
-          <button>Doctor</button>
-          <button>Administrador</button>
-          <button>SuperAdministrador</button>
+    <div className="botones">
+      <button className="botonInicio">
+        <img src={paciente} />
+        Paciente
+        <p>Solicita y gestiona tus citas médicas</p>
+      </button>
+
+      <button className="botonInicio">
+        <img src={doctor} />
+        Doctor
+        <p>Consulta tu agenda y disponibilidad</p>
+      </button>
+
+      <button className="botonInicio">
+        <img src={admin} />
+        Administrador
+        <p>Gestiona citas, pacientes y médicos</p>
+      </button>
+
+      <button className="botonInicio">
+        <img src={sup} />
+        Superadministrador
+        <p>Configura reglas de negocio y parámetros</p>
+      </button>
+    </div>
+              
+
         </div>
       </div>
-    </div>
+    
   );
 }
 
