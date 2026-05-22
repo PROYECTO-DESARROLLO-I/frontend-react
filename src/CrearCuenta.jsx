8263832811636d3a789855d1d3ed8b9bc34e1728
DarkXPrincess <veronica.granados@correounivalle.edu.co>
2026-05-22 14:47:00 -0500

    formulario crear cuenta

--- src/CrearCuenta.jsx
+++ src/CrearCuenta.jsx
@@ -1,8 +1,10 @@
 import "./App.css";
 import Card from "./Card";
 import { GoArrowLeft } from "react-icons/go";
+import { useState } from "react";
 
 function CrearCuenta({ volverLogin }) {
+    const [fecha, setFecha] = useState("");
   return (
     <div className="comp">
 
@@ -32,6 +34,35 @@ function CrearCuenta({ volverLogin }) {
             <label>Contraseña</label>
             <input type="password" placeholder="••••••••" />
 
+            <label>Tipo de documento</label>
+            <select>
+                <option value="">Selecciona un tipo de documento</option>
+                <option value="cc">Cédula de Ciudadanía</option>
+                <option value="ce">Cédula de Extranjería</option>
+                <option value="ti">Tarjeta de Identidad</option>
+                <option value="pasaporte">Pasaporte</option>
+                <option value="PPT">PPT</option>
+                <option value="PEP">PEP</option>
+            </select>
+
+            <label>Numero de documento</label>
+            <input type="text" placeholder="12679436" maxLength={10} onInput={(e) => {
+    e.target.value = e.target.value.replace(/[^0-9]/g, "");
+  }}/>
+
+            <label>Fecha de nacimiento</label>
+            <input type="date" value={fecha} onChange={(e)=> setFecha(e.target.value)} max={new Date().toISOString().split("T")[0]} />
+
+            <label>Numero de telefono</label>
+            <input type="text" placeholder="315487652" maxLength={10}/>
+
+            <label>Dirección</label>
+            <input type="text" placeholder="cl 33f #34-15" />
+
+            <label>EPS</label>
+            <select>
+                <option value="">Selecciona un tipo EPS</option>
+                </select>
           </div>
 
           <button className="enviar">
